315be810fab35369d0b6f8637cffbc086f4fc98b
zoiska <zoisk64@gmail.com>
2025-05-25 22:33:56 +0200

    fix import order

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { useAuth } from "./authContext.jsx";
 import BottomNav from "./components/BottomNav/BottomNav.jsx";
 import Home from "./pages/Home/Home.jsx";
 import Login from "./pages/Login/Login.jsx";
 import Profile from "./pages/Profile/Profile.jsx";
 import RecipeBrowser from "./pages/RecipeBrowser/RecipeBrowser.jsx";
 import Register from "./pages/Register/Register.jsx";
-import { useAuth } from "./authContext.jsx";
 
 import "./App.css";
 
