d45dace18be6c1410d6017e93509474ef668afdb
Seramas <jonas.cz@gmx.de>
2025-06-09 10:43:59 +0200

    fixed some UI issues

--- src/pages/CreateRecipe/CreateRecipe.jsx
+++ src/pages/CreateRecipe/CreateRecipe.jsx
@@ -64,129 +64,129 @@ function CreateRecipe() {
   const handleImageChange = () => {};
 
   return (
-    <div>
-      <div className="create-recipe-header">
-        <h1 className="create-recipe-title">Create A Recipe</h1>
-      </div>
+    <>
+      <div className="create-recipe-wrapper">
+        <h1 className="title">Create A Recipe</h1>
 
-      <div className="create-recipe-body">
-        <form className="create-recipe-form">
-          <div className="form-group">
-            <label htmlFor="recipe-title">Recipe Title</label>
-            <input
-              className="recipe-title"
-              type="text"
-              placeholder="Kartoffelgratin"
-              minLength="1"
-              required
-            />
-          </div>
-
-          <div className="form-group">
-            <div className="ingredients-buttons">
-              <label htmlFor="recipe-ingredients">Ingredients</label>
-              <button
-                className="removeingredient secondary-button"
-                type="button"
-                onClick={removeingredient}
-              >
-                -
-              </button>
-              <button
-                className="addingredient secondary-button"
-                type="button"
-                onClick={addingredient}
-              >
-                +
-              </button>
-            </div>
-            {ingredients.map((ingredient, index) => (
-              <div className="group-ingredients" key={index}>
-                <input
-                  className="recipe-ingredients-data"
-                  type="text"
-                  placeholder="Ingredient"
-                  minLength="1"
-                  required
-                  value={ingredient.name}
-                  onChange={(e) => {
-                    handleIngredientChange(index, "name", e.target.value);
-                  }}
-                />
-                <input
-                  className="recipe-ingredients-data"
-                  type="text"
-                  placeholder="Amount"
-                  minLength="1"
-                  required
-                  value={ingredient.amount}
-                  onChange={(e) => {
-                    handleIngredientChange(index, "amount", e.target.value);
-                  }}
-                />
-              </div>
-            ))}
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="recipe-instructions">Instructions</label>
-            <textarea
-              className="recipe-instructions"
-              placeholder="Instructions"
-              minLength="1"
-              required
-            />
-          </div>
-
-          <div className="form-group">
-            <div className="recipe-image">
-              <label htmlFor="recipe-image">Images</label>
-              <button
-                className="recipe-image-upload-button secondary-button"
-                type="button"
-                onClick={handleImageButtonClick}
-              >
-                <ArchiveRestore absoluteStrokeWidth={1} size={28} color="var(--color-text)" />
-              </button>
+        <div className="create-recipe-body">
+          <form className="create-recipe-form">
+            <div className="form-group">
+              <label htmlFor="recipe-title">Recipe Title</label>
               <input
-                className="upload-button"
-                type="file"
-                multiple
-                accept="image/*"
-                ref={fileInputRef}
-                onChange={handleImageChange}
+                className="recipe-title"
+                type="text"
+                placeholder="Kartoffelgratin"
+                minLength="1"
+                required
               />
             </div>
-            <div className="image-preview"></div>
-          </div>
-        </form>
-      </div>
 
-      {showPopup && (
-        <div className="popup-overlay">
-          <div className="popup">
-            Are you sure you want to cancel?
-            <div className="popup-buttons">
-              <button className="popup-cancel-buttons secondary-button" onClick={handleNo}>
-                No
-              </button>
-              <button className="popup-cancel-buttons primary-button" onClick={handleYes}>
-                Yes
-              </button>
+            <div className="form-group">
+              <div className="ingredients-buttons">
+                <label htmlFor="recipe-ingredients">Ingredients</label>
+                <button
+                  className="removeingredient secondary-button"
+                  type="button"
+                  onClick={removeingredient}
+                >
+                  -
+                </button>
+                <button
+                  className="addingredient secondary-button"
+                  type="button"
+                  onClick={addingredient}
+                >
+                  +
+                </button>
+              </div>
+              {ingredients.map((ingredient, index) => (
+                <div className="group-ingredients" key={index}>
+                  <input
+                    className="recipe-ingredients-data"
+                    type="text"
+                    placeholder="Ingredient"
+                    minLength="1"
+                    required
+                    value={ingredient.name}
+                    onChange={(e) => {
+                      handleIngredientChange(index, "name", e.target.value);
+                    }}
+                  />
+                  <input
+                    className="recipe-ingredients-data"
+                    type="text"
+                    placeholder="Amount"
+                    minLength="1"
+                    required
+                    value={ingredient.amount}
+                    onChange={(e) => {
+                      handleIngredientChange(index, "amount", e.target.value);
+                    }}
+                  />
+                </div>
+              ))}
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="recipe-instructions">Instructions</label>
+              <textarea
+                className="recipe-instructions"
+                placeholder="Instructions"
+                minLength="1"
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <div className="recipe-image">
+                <label htmlFor="recipe-image">Images</label>
+                <button
+                  className="recipe-image-upload-button secondary-button"
+                  type="button"
+                  onClick={handleImageButtonClick}
+                >
+                  <ArchiveRestore absoluteStrokeWidth={1} size={28} color="var(--color-text)" />
+                </button>
+                <input
+                  className="upload-button"
+                  type="file"
+                  multiple
+                  accept="image/*"
+                  ref={fileInputRef}
+                  onChange={handleImageChange}
+                />
+              </div>
+              <div className="image-preview"></div>
+            </div>
+          </form>
+        </div>
+
+        {showPopup && (
+          <div className="popup-overlay">
+            <div className="popup">
+              Are you sure you want to cancel?
+              <div className="popup-buttons">
+                <button className="popup-cancel-buttons secondary-button" onClick={handleNo}>
+                  No
+                </button>
+                <button className="popup-cancel-buttons primary-button" onClick={handleYes}>
+                  Yes
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
 
-      <div className="bottom-nav-create-recipe">
-        <button className="cancel-button" onClick={handleCancelEvent}>
-          <X size={32} color="var(--color-primary)" absoluteStrokeWidth={1} />
-        </button>
-        <button className="create-button" type="button" onClick={create}>
-          <ArrowUpFromLine size={32} color="var(--color-primary)" absoluteStrokeWidth={1} />
-        </button>
+        <div className="bottom-nav-create-recipe">
+          <button className="cancel-button" onClick={handleCancelEvent}>
+            <X size={32} color="var(--color-primary)" absoluteStrokeWidth={1} />
+          </button>
+          <button className="create-button" type="button" onClick={create}>
+            <ArrowUpFromLine size={32} color="var(--color-primary)" absoluteStrokeWidth={1} />
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
