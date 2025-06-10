51332a70ea1aa8d7cc049a9b069944bd57c05b55
Seramas <jonas.cz@gmx.de>
2025-06-10 09:00:34 +0200

    fixed import order and minor design changes

--- src/pages/CreateRecipe/CreateRecipe.jsx
+++ src/pages/CreateRecipe/CreateRecipe.jsx
@@ -1,5 +1,5 @@
-import { useState, useRef } from "react";
 import { ArrowUpFromLine, X, ArchiveRestore } from "lucide-react";
+import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import "./CreateRecipe.css";
 
